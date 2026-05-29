07705caf1bc872e3840823da14af22d0a70eedf1
Christiam Ramos <christiam.eramos@gmail.com>
2026-05-29 15:40:39 -0600

    Se incrementa el ancho a 50

--- SistemaContable.UI/Forms/Proveedores/frmConsultaQuedan.Designer.cs
+++ SistemaContable.UI/Forms/Proveedores/frmConsultaQuedan.Designer.cs
@@ -182,7 +182,7 @@ namespace SistemaContable.UI.Forms.Proveedores
             this.colEDITAR.ShowButtonMode = DevExpress.XtraGrid.Views.Base.ShowButtonModeEnum.ShowAlways;
             this.colEDITAR.Visible = true;
             this.colEDITAR.VisibleIndex = 1;
-            this.colEDITAR.Width = 40;
+            this.colEDITAR.Width = 50;
             // 
             // riEditar
             // 
@@ -206,7 +206,7 @@ namespace SistemaContable.UI.Forms.Proveedores
             this.colVER_Q.ShowButtonMode = DevExpress.XtraGrid.Views.Base.ShowButtonModeEnum.ShowAlways;
             this.colVER_Q.Visible = true;
             this.colVER_Q.VisibleIndex = 2;
-            this.colVER_Q.Width = 40;
+            this.colVER_Q.Width = 50;
             // 
             // riVerQ
             // 
@@ -229,7 +229,7 @@ namespace SistemaContable.UI.Forms.Proveedores
             this.colVER_R.ShowButtonMode = DevExpress.XtraGrid.Views.Base.ShowButtonModeEnum.ShowAlways;
             this.colVER_R.Visible = true;
             this.colVER_R.VisibleIndex = 3;
-            this.colVER_R.Width = 40;
+            this.colVER_R.Width = 50;
             // 
             // riVerR
             // 
@@ -251,7 +251,7 @@ namespace SistemaContable.UI.Forms.Proveedores
             this.colNOMBRE_ENTIDAD.OptionsColumn.AllowEdit = false;
             this.colNOMBRE_ENTIDAD.Visible = true;
             this.colNOMBRE_ENTIDAD.VisibleIndex = 4;
-            this.colNOMBRE_ENTIDAD.Width = 252;
+            this.colNOMBRE_ENTIDAD.Width = 242;
             // 
             // colTIPO_DTE
             // 
@@ -262,7 +262,7 @@ namespace SistemaContable.UI.Forms.Proveedores
             this.colTIPO_DTE.OptionsColumn.AllowEdit = false;
             this.colTIPO_DTE.Visible = true;
             this.colTIPO_DTE.VisibleIndex = 5;
-            this.colTIPO_DTE.Width = 61;
+            this.colTIPO_DTE.Width = 57;
             // 
             // colCOD_GENERACION
             // 
@@ -273,7 +273,7 @@ namespace SistemaContable.UI.Forms.Proveedores
             this.colCOD_GENERACION.OptionsColumn.AllowEdit = false;
             this.colCOD_GENERACION.Visible = true;
             this.colCOD_GENERACION.VisibleIndex = 6;
-            this.colCOD_GENERACION.Width = 195;
+            this.colCOD_GENERACION.Width = 188;
             // 
             // colSALDO
             // 
@@ -289,7 +289,7 @@ namespace SistemaContable.UI.Forms.Proveedores
             this.colSALDO.OptionsColumn.AllowEdit = false;
             this.colSALDO.Visible = true;
             this.colSALDO.VisibleIndex = 7;
-            this.colSALDO.Width = 119;
+            this.colSALDO.Width = 112;
             // 
             // colFECHA_EMISION
             // 
@@ -306,7 +306,7 @@ namespace SistemaContable.UI.Forms.Proveedores
             this.colFECHA_EMISION.OptionsColumn.AllowEdit = false;
             this.colFECHA_EMISION.Visible = true;
             this.colFECHA_EMISION.VisibleIndex = 8;
-            this.colFECHA_EMISION.Width = 119;
+            this.colFECHA_EMISION.Width = 112;
             // 
             // colFECHA_RECIBIDO
             // 
@@ -322,7 +322,7 @@ namespace SistemaContable.UI.Forms.Proveedores
             this.colFECHA_RECIBIDO.ShowButtonMode = DevExpress.XtraGrid.Views.Base.ShowButtonModeEnum.ShowAlways;
             this.colFECHA_RECIBIDO.Visible = true;
             this.colFECHA_RECIBIDO.VisibleIndex = 9;
-            this.colFECHA_RECIBIDO.Width = 236;
+            this.colFECHA_RECIBIDO.Width = 241;
             // 
             // frmConsultaQuedan
             // 
